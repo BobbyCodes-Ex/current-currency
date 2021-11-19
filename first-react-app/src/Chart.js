9a60b118193d4caa35bf121f48605df0b8881c3e
Wesley Wood <wesleyowood@gmail.com>
2021-11-19 15:42:13 -0700

    Routes 2

--- first-react-app/src/Chart.js
+++ first-react-app/src/Chart.js
@@ -11,7 +11,24 @@ class Chart extends React.Component {
                         <div className="text-center">
                             <div className="title">Chart</div> 
                         </div>
-                        <div className="">Wes</div>
+                        <div className="currency-choice text-center">
+                            <form className="text-center">
+                                <div class="input-group mb-3">
+                                    <label class="input-group-text" for="inputGroupSelect01">Currency</label>
+                                    <select class="form-select" id="inputGroupSelect01">
+                                        <option selected>Choose...</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                    <button type="button" className="btn btn-primary btn-sm">Add</button>
+                                </div>
+                            </form>
+                            <div className="row">
+                                <div className="col col-6-lg chart-title">Currency</div>
+                                <div className="col col-6-lg chart-title">Rate</div>
+                            </div>
+                        </div>
                       </div>
                   </div>
               </div>
--- first-react-app/src/Exchange.js
+++ first-react-app/src/Exchange.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './Exchange.css';
 
 class Exchange extends React.Component {
+
+   
+
+
     render () {
         return (
             <div>
@@ -11,7 +15,59 @@ class Exchange extends React.Component {
                         <div className="text-center">
                             <div className="title">Exchange</div> 
                         </div>
-                        <div className="">Wes</div>
+                        <div className="row text-center">
+                                <div className="col col-6-lg exchange-title">My Currency</div>
+                                <div className="col col-6-lg exchange-title">Currency I Want</div>
+                            </div>
+                        <div className="row">
+                            <div className="col col-lg-6">
+                                <div class="input-group mb-3">
+                                    <label class="input-group-text" for="inputGroupSelect01">Currency</label>
+                                    <select class="form-select" id="inputGroupSelect01">
+                                        <option selected>Choose...</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="col col-lg-6">
+                                <div class="input-group mb-3">
+                                    <label class="input-group-text" for="inputGroupSelect01">Currency</label>
+                                    <select class="form-select" id="inputGroupSelect01">
+                                        <option selected>Choose...</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <i class='fas fa-equals equal-symbol'></i>
+                        <div className="row">
+                            <div className="col col-lg-6">
+                                <div class="input-group mb-3">
+                                    <label class="input-group-text" for="inputGroupSelect01">Amount</label>
+                                    <select class="form-select" id="inputGroupSelect01">
+                                        <option selected>Choose...</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="col col-lg-6">
+                                <div class="input-group mb-3">
+                                    <label class="input-group-text" for="inputGroupSelect01">Amount</label>
+                                    <select class="form-select" id="inputGroupSelect01">
+                                        <option selected>Choose...</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                       </div>
                   </div>
               </div>
